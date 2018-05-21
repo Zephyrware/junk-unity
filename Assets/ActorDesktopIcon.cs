c6734f0bec700a73cfe537f14b9ff6dd2878de42
Travis Scott <travisscott301@gmail.com>
2018-05-21 17:48:15 -0500

    Added basic program opening

---
+++ Assets/ActorDesktopIcon.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ActorDesktopIcon : MonoBehaviour, IPointerDownHandler {
+
+    [SerializeField] private ActorProgram _program;
+
+    int clicked = 0;
+    float clicktime = 0;
+    float clickdelay = 0.5f;
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        clicked++;
+        if (clicked == 1) clicktime = Time.time;
+
+        if (clicked > 1 && Time.time - clicktime < clickdelay)
+        {
+            clicked = 0;
+            clicktime = 0;
+            DoubleClick();
+
+        }
+        else if (clicked > 2 || Time.time - clicktime > 1) clicked = 0;
+
+    }
+
+    void DoubleClick() {
+        var item = Instantiate(_program);
+        var canvas = GameManager.Canvas;
+        item.transform.SetParent(canvas.transform);
+        item.GetComponent<RectTransform>().localPosition = Vector3.zero;
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameObject Canvas;
+
+    private void Awake()
+    {
+        Canvas = GameObject.Find("Canvas");
+    }
+}
